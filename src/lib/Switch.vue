c9b3b94a2855eb65cbc596448f5f24e11bc8ab15
joli <1040123159@qq.com>
2020-10-13 20:57:28 +0800

    动画流畅了

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -37,6 +37,7 @@ span {
   width: $h2;
   background: white;
   border-radius: $h2/2;
+  transition: left 250ms;
 }
 button.checked{
   background-color: blue;
@@ -44,5 +45,7 @@ button.checked{
 button.checked span{
   left: calc(100% - #{$h2} - 2px);
 }
-
+button:focus{
+  outline: none;
+}
 </style>
