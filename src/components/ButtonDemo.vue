94b6ef39fcd9d7d1677714df42e455170fb9a181
joli <1040123159@qq.com>
2020-10-14 15:16:37 +0800

    创建button组件

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,12 +1,23 @@
 <template>
   <div>
-    <Button>123</Button>
+    <Button
+      @click="a"
+    @focus="a"
+    @mouseover="a"
+    size="small">123</Button>
   </div>
 </template>
 
 <script lang="ts">
 import Button from '../lib/Button.vue'
 export default {
-  components:Button
+  components:{Button},
+  setup(){
+    const a=()=>{
+      console.log(1);
+    }
+    return{a}
+  }
+
 };
 </script>
--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,8 +1,23 @@
 <template>
-  <button>
-    <slot/>
-  </button>
+  <div>
+    <button v-bind="$attrs">
+      <slot/>
+    </button>
+  </div>
+
 </template>
+
+<script lang="ts">
+export default {
+  inheritAttrs:false,
+}
+</script>
+
+<style lang="scss" scoped>
+div{
+  border: 1px solid red;
+}
+</style>
 
 
 
