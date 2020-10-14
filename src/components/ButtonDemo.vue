973ffcc6c82c5f6d750332bf748afa2772504d36
joli <1040123159@qq.com>
2020-10-14 21:52:27 +0800

    支持size 和 theme属性

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -4,7 +4,11 @@
     <Button theme="button">你好</Button>
     <Button theme="link">你好</Button>
     <Button theme="text">你好</Button>
-
+  </div>
+  <div>
+    <Button size="big">大大大</Button>
+    <Button >正常</Button>
+    <Button size="small">小小小</Button>
   </div>
 </template>
 
--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,21 +1,37 @@
 <template>
-  <button class="dy-button" :class="{[`dy-theme-${theme}`]:theme}">
+  <button class="dy-button" :class="classes">
     <slot/>
   </button>
 </template>
 
 <script lang="ts">
+import {computed} from 'vue';
+
 export default {
   props: {
     theme: {
       type: String,
       default: 'button'
+    },
+    size: {
+      type: String,
+      default: 'normal'
     }
-  }
+  },
+  setup(props) {
+    const {theme, size} = props;
+    const classes = computed(() => {
+      return {
+        [`dy-theme-${theme}`]: theme,
+        [`dy-size-${size}`]: size,
+      };
+    });
+    return {classes};
+  },
 };
 </script>
 
-<style lang="scss" >
+<style lang="scss">
 $h: 32px;
 $border-color: #d9d9d9;
 $color: #333;
@@ -52,6 +68,36 @@ $radius: 4px;
 
   &::-moz-focus-inner {
     border: 0;
+  }
+  &.dy-theme-link{
+    border-color: transparent;
+    box-shadow: none;
+    color: $blue;
+    &:hover,
+    &:focus{
+      color: lighten($blue, 10%);
+    }
+  }
+  &.dy-theme-text {
+    border-color: transparent;
+    box-shadow: none;
+    color: inherit;
+    &:hover,
+    &:focus {
+      background: darken(white, 5%);
+    }
+  }
+  &.dy-theme-button{
+    &.dy-size-big{
+      font-size: 24px;
+      height: 48px;
+      padding: 0 16px
+    }
+    &.dy-size-small{
+      font-size: 12px;
+      height: 20px;
+      padding: 0 4px;
+    }
   }
 }
 </style>
