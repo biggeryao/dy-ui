9ac2159a56b0c9f566ef4625bf7fd00ea90f0f31
joli <1040123159@qq.com>
2020-10-15 16:12:34 +0800

    使用css切换内容

--- src/components/TabsDemo.vue
+++ src/components/TabsDemo.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
     Tab示例
-  <Tabs>
-    <Tab title="导航一">内容一</Tab>
-    <Tab title="导航二">内容一</Tab>
+  <Tabs v-model:selected="x">
+    <Tab title="导航1">内容一</Tab>
+    <Tab title="导航2">内容2</Tab>
   </Tabs>
   </div>
 </template>
@@ -11,7 +11,16 @@
 <script lang="ts">
 import Tab from '../lib/Tab.vue'
 import Tabs from  '../lib/Tabs.vue'
+import {
+  ref
+} from 'vue'
 export default {
-  components:{Tabs,Tab}
+  components:{Tabs,Tab},
+  setup() {
+    const x = ref('导航2')
+    return {
+      x
+    }
+  }
 }
 </script>
--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,25 +1,84 @@
 <template>
-  <div>
-    <div v-for="(t,index) in titles" :key="index">{{t}}</div>
-    <component v-for="(c,index) in defaults" :is="c" :key="index"/>
+  <div class="dy-tabs">
+    <div class="dy-tabs-nav">
+      <div
+          class="dy-tabs-nav-item"
+          v-for="(t,index) in titles"
+          :key="index"
+          @click="select(t)"
+          :class="{selected:t===selected}">{{ t }}</div>
+    </div>
+    <div class="dy-tabs-content">
+      <component class="dy-tabs-content-item"
+                 v-for="(c,index) in defaults" :key="index"
+                 :class="{selected:c.props.title===selected}"
+                  :is="c" />
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import Tab from './Tab.vue'
+import Tab from './Tab.vue';
+import {computed} from 'vue'
+
 export default {
-  components:{Tab},
-  setup(props,context){
-    const defaults=context.slots.default()
-    defaults.forEach((tag)=>{
-      if(tag.type!==Tab){
-        throw new Error('Tabs子组件必须是Tab')
+  props:{
+    selected:{
+      type:String
+    }
+  },
+  components: {Tab},
+  setup(props, context) {
+    const defaults = context.slots.default();
+    defaults.forEach((tag) => {
+      if (tag.type !== Tab) {
+        throw new Error('Tabs子组件必须是Tab');
       }
+    });
+    const titles = defaults.map((tag) => {
+      return tag.props.title;
+    });
+    const current=computed(()=>{
+      return defaults.filter((tag)=>{
+        return tag.props.title===props.selected
+      })[0]
     })
-    const titles=defaults.map((tag)=>{
-      return tag.props.title
-    })
-    return{defaults,titles}
+    const select=(title:string)=>{
+      context.emit('update:selected',title)
+    }
+    return {defaults, titles,current,select};
+  }
+};
+</script>
+<style lang="scss">
+$blue: #40a9ff;
+$color: #333;
+$border-color: #d9d9d9;
+.dy-tabs {
+  &-nav {
+    display: flex;
+    color: $color;
+    border-bottom: 1px solid $border-color;
+    &-item {
+      padding: 8px 0;
+      margin: 0 16px;
+      cursor: pointer;
+      &:first-child {
+        margin-left: 0;
+      }
+      &.selected {
+        color: $blue;
+      }
+    }
+  }
+  &-content {
+    padding: 8px 0;
+    &-item {
+      display: none;
+      &.selected{
+        display: block;
+      }
+    }
   }
 }
-</script>
+</style>
