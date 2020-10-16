1eb060a88b16528dd942b9ad99608cc63bcd36d2
joli <1040123159@qq.com>
2020-10-16 15:34:38 +0800

    展示高亮代码

--- src/components/Demo.vue
+++ src/components/Demo.vue
@@ -1,37 +1,39 @@
 <template>
-  <div>
-
-    <h1>Switch 组件示例 </h1>
-    <Demo :component="Switch1Demo" title="常规用法"/>
-    <Demo :component="Switch2Demo" title="支持disable"/>
+  <div class="demo">
+    <h2>{{title}}</h2>
+    <div class="demo-component">
+      <component :is="component" />
+    </div>
+    <div class="demo-actions">
+      <Button @click="codeVisible=!codeVisible">查看代码</Button>
+    </div>
+    <div class="demo-code" v-if="codeVisible">
+      <pre class="language-html" v-html="html" />
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import Button from '../lib/Button.vue'
-import Switch1Demo from './Switch1.demo.vue'
-import Switch2Demo from './Switch2.demo.vue'
 import 'prismjs';
 import 'prismjs/themes/prism.css'
 const Prism = (window as any).Prism
-import {ref} from 'vue'
-import Demo from './Demo.vue'
+import {computed,ref} from 'vue'
 export default {
-  components: {
-    Button,   Switch1Demo, Switch2Demo,Demo
+  props:{
+    component:Object,
+    title:String
   },
-  setup() {
-    const bool = ref(false)
-    return {
-      bool,
-      Prism,
-      Switch1Demo,
-      Switch2Demo,
-    }
+  components:{Button},
+  setup(props){
+    const html= computed(()=>{
+      return Prism.highlight(props.component.__sourceCode,Prism.languages.html,'html')
+    })
+    const codeVisible =ref(false)
+    return {Prism,html,codeVisible}
   }
 }
 </script>
-
 <style lang="scss" scoped>
 $border-color: #d9d9d9;
 .demo {
--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -23,38 +23,19 @@ export default {
 $h: 22px;
 $h2: $h - 4px;
 .dy-switch {
-  height: $h;
-  width: $h*2;
-  border: none;
-  background-color: gray;
-  border-radius: $h/2;
-  position: relative;
-
-}
-
-span {
-  position: absolute;
-  top: 2px;
-  left: 2px;
-  height: $h2;
-  width: $h2;
-  background: white;
-  border-radius: $h2/2;
-  transition: left 250ms;
-}
-.dy-switch.dy-checked{
-  background-color: blue;
-}
-.dy-switch.dy-checked span{
-  left: calc(100% - #{$h2} - 2px);
-}
-.dy-switch:focus{
-  outline: none;
-}
-.dy-switch:active{
-  > span {width: $h2 + 4px;}
-}
-.dy-switch.dy-checked:active{
-  > span {width: $h2 + 4px; margin-left: -4px;}
+  height: $h; width: $h * 2; border: none; background: #bfbfbf; border-radius: $h/2; position: relative;
+  > span {
+    position: absolute; top: 2px; left: 2px; height: $h2; width: $h2; background: white; border-radius: $h2 / 2; transition: all 250ms;
+  }
+  &.dy-checked { background: #1890ff;
+    > span { left: calc(100% - #{$h2} - 2px); }
+  }
+  &:focus { outline: none; }
+  &:active {
+    > span { width: $h2 + 4px; }
+  }
+  &.dy-checked:active {
+    > span { width: $h2 + 4px; margin-left: -4px; }
+  }
 }
 </style>
